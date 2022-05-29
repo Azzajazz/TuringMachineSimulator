8f73f5cbd089db67240740b07a6ddf14c661a9fa
Adam Rowland <u5799578@anu.edu.au>
2022-05-30 05:58:33 +1000

    Playing around with different implementations

--- src/one_package/gui.py
+++ src/one_package/gui.py
@@ -2,17 +2,33 @@ from cgitb import grey
 import tkinter as tk
 from tkinter import ttk
 
-class Window(tk.Tk):
-  def __init__(self, **kwargs):
+class EditorCanvas(tk.Canvas):
+  def __init__(self, parent, **kwargs):
     super().__init__(**kwargs)
-    self.tm_canvas = tk.Canvas(self, background="grey", width=300, height=300)
-    self.tm_canvas.grid(row=0, column=1)
-    self.sidebar = tk.Frame(self, width=100, height=300, background="red")
-    self.sidebar.grid(row=0, column=0)
-  
+    # Some other state goes in here. Not sure what yet
+
+  # Also some event handlers and such. Maybe abstract them to different classes if they get too unwieldy?  
+
+class EditorWindow():
+  def __init__(self, **kwargs):
+    self.window_width = 600
+    self.window_height = 500
+    self.root = tk.Tk()
+    self.editor_canvas = EditorCanvas(self.root, width=self.window_width // 3 * 2, height=self.window_height, background="grey")
+    self.editor_canvas.grid(row=0, column=1)
+    self.options_frame = tk.Frame(self.root, width=self.window_width // 3, height=self.window_height, background="red")
+    self.options_frame.grid(row=0, column=0)
+    self.root.bind("<Configure>", self.resize)
+
+  '''Running of the application'''
   def run(self):
-    self.mainloop()
+    self.root.mainloop()
+
+  '''Event on window resize'''
+  def resize(self, event):
+    if event.widget == self.root and (event.width != self.window_width or event.height != self.window_height):
+      self.window_width, self.window_height = event.width, event.height
 
 if __name__ == "__main__":
-  editor = Window()
+  editor = EditorWindow()
   editor.run()

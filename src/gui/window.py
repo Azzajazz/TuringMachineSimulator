3bd35cdc31e33dc95a4680bbbd6c32bd5355259b
Adam Rowland <u5799578@anu.edu.au>
2022-06-25 17:51:02 +1000

    Finished initial view framework

--- src/gui/window.py
+++ src/gui/window.py
@@ -4,29 +4,53 @@ class Window(tk.Tk):
   def __init__(self, width=900, height=500, **kwargs):
     super().__init__(**kwargs)
 
-    # Main layout of the gui. Canvas on the right, options on the left
+    self.build_window()
+    self.geometry(f"{width}x{height}")
+
+  def build_window(self):
+    self.build_editor_canvas()
+    self.build_options_frame()
+
+  def build_editor_canvas(self):
     self.editor_canvas = tk.Canvas(self, bg="grey")
     self.editor_canvas.pack(side="right", fill="both", expand=True)
-    options_frame = tk.Frame(self)
-    options_frame.pack(side="left", fill="y", expand=False)
 
-    tk.Label(options_frame, text="Tape").pack()
-    tape_slots_frame = tk.Frame(options_frame)
-    tape_slots_frame.pack(fill="x")
+  def build_options_frame(self):
+    options_frame = tk.Frame(self)
+    options_frame.pack(side="left", fill="y", ipadx=10)
+    self.build_tape_frame(options_frame)
+    self.build_debug_frame(options_frame)
+    self.build_description_frame(options_frame)
+
+  def build_tape_frame(self, parent):
+    tape_frame = tk.Frame(parent)
+    tape_frame.pack(pady=10)
+    tk.Label(tape_frame, text="Tape").pack()
+    tape_slots_frame = tk.Frame(tape_frame)
+    tape_slots_frame.pack()
     self.tape_slots_entries = [
       tk.Entry(tape_slots_frame, width=3) for _ in range(7)
     ]
     for entry in self.tape_slots_entries:
       entry.pack(side="left")
-    self.tape_slots_entries[0].insert(0, "Hello!")
-    self.tape_slots_entries[1].insert(0, "Goodbye!")
-    # self.tape_entr.insert(0, "This will be a tape")
-    # self.tape_entry.pack(side="top", fill="x")
-    self.tape_head_label = tk.Label(options_frame, text="/\\")
-    self.tape_head_label.pack(side="top", fill="x")
+    self.tape_head_label = tk.Label(tape_frame, text="/\\")
+    self.tape_head_label.pack()
 
-    # Buttons are organised in their own frame.
-    debug_button_frame = tk.Frame(options_frame)
+  def build_debug_frame(self, parent):
+    debug_frame = tk.Frame(parent)
+    debug_frame.pack()
+    self.build_input_frame(debug_frame)
+    self.build_debug_button_frame(debug_frame)
+  
+  def build_input_frame(self, parent):
+    input_frame = tk.Frame(parent)
+    input_frame.pack(fill="x")
+    tk.Label(input_frame, text="Input:").pack(side="left")
+    self.input_entry = tk.Entry(input_frame)
+    self.input_entry.pack(side="left", fill="x")
+
+  def build_debug_button_frame(self, parent):
+    debug_button_frame = tk.Frame(parent)
     debug_button_frame.pack()
     self.run_button = tk.Button(debug_button_frame, text="Run")
     self.run_button.pack(side="left")
@@ -37,30 +61,24 @@ class Window(tk.Tk):
     self.stop_button = tk.Button(debug_button_frame, text="Stop")
     self.stop_button.pack(side="left")
     
-    self.alpha_label = tk.Label(options_frame, text="Alphabet = {a, b}")
-    self.alpha_label.pack()
-    self.alpha_button = tk.Button(options_frame, text="Set Alphabet")
-    self.alpha_button.pack()
-    self.state_label = tk.Label(options_frame, text="Q = {q0, q1}")
-    self.state_label.pack()
-    self.finals_label = tk.Label(options_frame, text="F = {q1}")
-    self.finals_label.pack()
-    self.delta_label_1 = tk.Label(options_frame, text="delta(q0, a) = (q1, a, L)")
-    self.delta_label_1.pack()
-    self.delta_label_2 = tk.Label(options_frame, text="delta(q1, b) = (q0, a, L)")
-    self.delta_label_2.pack()
+  def build_description_frame(self, parent):
+    description_frame = tk.Frame(parent)
+    description_frame.pack(fill="x", side="bottom", pady=20, padx=10)
+    self.alpha_label = tk.Label(description_frame, text="Alphabet = {a, b}", justify="left")
+    self.alpha_label.grid(row=0, column=0, sticky="w")
+    self.alpha_button = tk.Button(description_frame, text="Set Alphabet")
+    self.alpha_button.grid(row=1, column=0, sticky="w")
+    self.state_label = tk.Label(description_frame, text="Q = {q0, q1}", justify="left")
+    self.state_label.grid(row=2, column=0, sticky="w")
+    self.finals_label = tk.Label(description_frame, text="F = {q1}", justify="left")
+    self.finals_label.grid(row=3, column=0, sticky="w")
+    self.delta_label_1 = tk.Label(description_frame, text="delta(q0, a) = (q1, a, L)", justify="left")
+    self.delta_label_1.grid(row=4, column=0, sticky="w")
+    self.delta_label_2 = tk.Label(description_frame, text="delta(q1, b) = (q0, a, L)", justify="left")
+    self.delta_label_2.grid(row=5, column=0, sticky="w")
+
     ### TODO: There's some more stuff that needs to go in this window, but not sure what yet. ###
 
-    # Sizing the initial window
-    self.geometry(f"{width}x{height}")
-
-  def on_step_click(self, _):
-    print("Clicked!")
-  
-  def clear_canvas(self, _):
-    self.editor_canvas.delete("all")
-
-  '''Running of the application'''
   def run(self):
     self.mainloop()
 

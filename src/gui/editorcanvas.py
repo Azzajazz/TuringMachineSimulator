c26a0e3218c2b5b0e2225b428e1bb00039714601
Adam Rowland <u5799578@anu.edu.au>
2022-06-30 21:19:42 +1000

    Auto-formatted with black. Modularised calculations in EditorCanvas class.

--- src/gui/editorcanvas.py
+++ src/gui/editorcanvas.py
@@ -1,113 +1,138 @@
 import tkinter as tk
 import math
 from dataclasses import dataclass
+from typing import NamedTuple
+
+
+class Point(NamedTuple):
+    x: float
+    y: float
+
 
 @dataclass
 class StateInfo:
-  center: tuple[float, float]
-  radius: float
+    centre: Point
+    radius: float
 
+
+### TODO: Modularise mathematical calculations for reuse in several event bindings
+### TODO: Figure out how to associate transitions with states in a way that makes maths simple.
 class EditorCanvas(tk.Canvas):
-  state_radius = 20
+    state_radius = 20
 
-  def __init__(self, parent, **kwargs):
-    super().__init__(parent, **kwargs)
-    self.id_to_info_map: dict[int, StateInfo] = {}
-    self.links: dict[int, int] = {}
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.id_to_info_map: dict[int, StateInfo] = {}
+        self.links: dict[int, int] = {}
 
-    self.bind("<Button-3>", self.begin_transition_draw)
-    self.bind("<B3-Motion>", self.update_transition)
-    self.bind("<ButtonRelease-3>", self.finalise_transition)
+        self.bind("<Button-3>", self.begin_transition_draw)
+        self.bind("<B3-Motion>", self.update_transition)
+        self.bind("<ButtonRelease-3>", self.finalise_transition)
 
-  def begin_transition_draw(self, event):
-    self.transition_start_x = event.x
-    self.transition_start_y = event.y
-    self.create_line(event.x, event.y, event.x, event.y, event.x, event.y,
-      tags="drawing", smooth=True, arrow="last")
-  
-  def update_transition(self, event):
-    x0, y0 = self.transition_start_x, self.transition_start_y
-    x1, y1 = event.x, event.y
-    if x1 >= x0 and y1 >= y0:
-      xmul, ymul = -1, 1
-    elif x1 >= x0 and y1 < y0:
-      xmul, ymul = 1, 1
-    elif x1 < x0 and y1 >= y0:
-      xmul, ymul = -1, -1
+    def begin_transition_draw(self, event):
+        self.transition_start_x = event.x
+        self.transition_start_y = event.y
+        self.create_line(
+            event.x,
+            event.y,
+            event.x,
+            event.y,
+            event.x,
+            event.y,
+            tags="drawing",
+            smooth=True,
+            arrow="last",
+        )
+
+    def update_transition(self, event):
+        curve_x, curve_y = calculate_curve_point(
+            Point(self.transition_start_x, self.transition_start_y),
+            Point(event.x, event.y),
+        )
+        self.coords(
+            "drawing",
+            self.transition_start_x,
+            self.transition_start_y,
+            curve_x,
+            curve_y,
+            event.x,
+            event.y,
+        )
+
+    def finalise_transition(self, _):
+        self.dtag("drawing", "drawing")
+
+        # TEMPORARY
+        # state1 = self.create_oval(10, 10, 50, 50)
+        # state1info = StateInfo(Point(30, 30), 20)
+        # self.id_to_info_map[state1] = state1info
+
+        # state2 = self.create_oval(10, 200, 50, 240)
+        # state2info = StateInfo(Point(30, 220), 20)
+        # self.id_to_info_map[state2] = state2info
+
+        # centre_x, centre_y = calculate_transition_anchor(
+        #     state1info.centre, state2info.centre, self.state_radius
+        # )
+
+        # self.create_oval(centre_x - 5, centre_y - 5, centre_x + 5, centre_y + 5)
+        # END TEMPORARY
+
+    def add_state(self, event):
+        x0 = (event.x - self.state_radius,)
+        y0 = (event.y - self.state_radius,)
+        x1 = (event.x + self.state_radius,)
+        y1 = event.y + self.state_radius
+        new_id = self.create_oval(x0, y0, x1, y1)
+        new_info = StateInfo(
+            center=(0.5 * (x0 + x1), 0.5 * (y0 + y1)), radius=0.5 * abs(x0 - x1)
+        )
+        self.id_to_info_map[new_id] = new_info
+
+
+def calculate_curve_point(p0: Point, p1: Point) -> tuple[float, float]:
+    """
+    Middle point for a curving transition: for transition starting at (x0, y0) and with head at (x1, y1),
+    the point that defines the curve of the transition is
+      (xmul * math.sqrt(r * r / (m * m + 1)) + xM, ymul * math.sqrt(r * r - r * r / (m * m + 1)) + yM)
+    where
+      xmul, ymul = +/-1 are determined based on the gradient of the line from (x0, y0) to (x1, y1)
+      r = c||(x0, y0) - (x1, y1)||, with c a constant (TBD, controls the strength of the curve)
+      m = (x1 - x0) / (y0 - y1)
+      (xM, yM) = (0.5 * (x0 + x1), 0.5 * (y0 + y1)) is the midpoint of the line segment from (x0, y0) to (x1, y1)
+
+    if y0 = y1, then m is undefined. In this case, the point that defines the curve of the transition is
+    (xM, yM +/- r)
+    """
+
+    xM, yM = 0.5 * (p0.x + p1.x), 0.5 * (p0.y + p1.y)
+    r = 0.4 * math.sqrt((p1.x - p0.x) ** 2 + (p1.y - p0.y) ** 2)
+    if p0.y == p1.y:
+        curve_x, curve_y = xM, yM + math.copysign(r, p1.x - p0.x)
     else:
-      xmul, ymul = 1, -1
-    xM, yM = 0.5 * (x0 + x1), 0.5 * (y0 + y1)
-    r = 0.4 * math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
-    if y0 == y1:
-      curve_x, curve_y = xM, yM + ymul * r
-    else:
-      m = (x0 - x1) / (y1 - y0)
-      curve_x = xmul * math.sqrt(r * r / (m * m + 1)) + xM
-      curve_y = ymul * math.sqrt(r * r - r * r / (m * m + 1)) + yM
-    self.coords(
-      "drawing",
-      x0, y0,
-      curve_x, curve_y,
-      x1, y1
-    )
-  
-  def finalise_transition(self, _):
-    self.dtag("drawing", "drawing")
+        m = (p0.x - p1.x) / (p1.y - p0.y)
+        curve_x = math.copysign(math.sqrt(r * r / (m * m + 1)), p0.y - p1.y) + xM
+        curve_y = (
+            math.copysign(math.sqrt(m * m * r * r / (m * m + 1)), p1.x - p0.x) + yM
+        )
+    return Point(curve_x, curve_y)
 
-    # #TEMPORARY
-    # state1 = self.create_oval(10, 10, 50, 50)
-    # state1info = StateInfo((30, 30), 20)
-    # self.id_to_info_map[state1] = state1info
-    
-    # state2 = self.create_oval(10, 200, 50, 240)
-    # state2info = StateInfo((30, 220), 20)
-    # self.id_to_info_map[state2] = state2info
 
-    # height = state2info.center[1] - state1info.center[1]
-    # width = state2info.center[0] - state1info.center[0]
-    # phi = math.atan2(height, width)
-    # center_x = 20 * math.cos(phi + 0.5) + state1info.center[0]
-    # center_y = 20 * math.sin(phi + 0.5) + state1info.center[1]
-    # self.create_oval(
-    #   center_x - 5,
-    #   center_y - 5,
-    #   center_x + 5,
-    #   center_y + 5
-    # )
-    # self.bind("<Button-1>", self.add_state)
-  
-  def add_state(self, event):
-    x0 = event.x - self.state_radius,
-    y0 = event.y - self.state_radius,
-    x1 = event.x + self.state_radius,
-    y1 = event.y + self.state_radius
-    new_id = self.create_oval(x0, y0, x1, y1)
-    new_info = StateInfo(
-      center=(0.5 * (x0 + x1), 0.5 * (y0 + y1)),
-      radius=0.5 * abs(x0 - x1)
-    )
-    self.id_to_info_map[new_id] = new_info
+def calculate_transition_anchor(p0: Point, p1: Point, radius: float) -> Point:
+    """
+    Transition anchor points: between circles with centers (x0, y0) and (x1, y1),
+    the point on the circumference where a transition will join is
+      (rcos(phi +/- theta) + x0, rsin(phi +/- theta) + y0)
+    where
+      theta is a fixed small angle (TBD)
+      phi = atan2(y1 - y0, x1 - x0)
+      r is the radius of the state (TBD)
+    """
 
-'''
-Transition anchor points: between circles with centers (x0, y0) and (x1, y1),
-the point on the circumference where a transition will join is
-  (rcos(phi +/- theta) + x0, rsin(phi +/- theta) + y0)
-where
-  theta is a fixed small angle (TBD)
-  phi = atan2(y1 - y0, x1 - x0)
-  r is the radius of the state (TBD)
-'''
+    height = p1.y - p0.y
+    width = p1.x - p0.x
+    phi = math.atan2(height, width)
+    centre_x = 20 * math.cos(phi + 0.5) + p0.x
+    centre_y = 20 * math.sin(phi + 0.5) + p0.y
+    return Point(centre_x, centre_y)
 
-'''
-Middle point for a curving transition: for transition starting at (x0, y0) and with head at (x1, y1),
-the point that defines the curve of the transition is
-  (xmul * math.sqrt(r * r / (m * m + 1)) + xM, ymul * math.sqrt(r * r - r * r / (m * m + 1)) + yM)
-where
-  xmul, ymul are determined based on the gradient of the line from (x0, y0) to (x1, y1)
-  r = c||(x0, y0) - (x1, y1)||, with c a constant (TBD, controls the strength of the curve)
-  m = (x1 - x0) / (y0 - y1)
-  (xM, yM) = (0.5 * (x0 + x1), 0.5 * (y0 + y1)) is the midpoint of the line segment from (x0, y0) to (x1, y1)
-
-if y0 = y1, then m is undefined. In this case, the point that defines the curve of the transition is
-(xM, yM + ymul * r)
-'''
--- src/gui/window.py
+++ src/gui/window.py
@@ -1,91 +1,103 @@
 import tkinter as tk
 from editorcanvas import EditorCanvas
 
+
 class Window(tk.Tk):
-  def __init__(self, width=900, height=500, **kwargs):
-    super().__init__(**kwargs)
+    def __init__(self, width=900, height=500, **kwargs):
+        super().__init__(**kwargs)
 
-    self.build_window()
-    self.geometry(f"{width}x{height}")
+        self.build_window()
+        self.geometry(f"{width}x{height}")
 
-  def build_window(self):
-    self.build_editor_canvas()
-    self.build_options_frame()
+    def build_window(self):
+        self.build_editor_canvas()
+        self.build_options_frame()
 
-  def build_editor_canvas(self):
-    self.editor_canvas = EditorCanvas(self, bg="grey")
-    self.editor_canvas.pack(side="right", fill="both", expand=True)
+    def build_editor_canvas(self):
+        self.editor_canvas = EditorCanvas(self, bg="grey")
+        self.editor_canvas.pack(side="right", fill="both", expand=True)
 
-  def build_options_frame(self):
-    options_frame = tk.Frame(self)
-    options_frame.pack(side="left", fill="y", ipadx=10)
-    self.build_tape_frame(options_frame)
-    self.build_debug_frame(options_frame)
-    self.build_description_frame(options_frame)
+    def build_options_frame(self):
+        options_frame = tk.Frame(self)
+        options_frame.pack(side="left", fill="y", ipadx=10)
+        self.build_tape_frame(options_frame)
+        self.build_debug_frame(options_frame)
+        self.build_description_frame(options_frame)
 
-  def build_tape_frame(self, parent):
-    tape_frame = tk.Frame(parent)
-    tape_frame.pack(pady=10)
-    tk.Label(tape_frame, text="Tape").pack()
-    tape_slots_frame = tk.Frame(tape_frame)
-    tape_slots_frame.pack()
-    self.tape_slots_entries = [
-      tk.Entry(tape_slots_frame, width=3) for _ in range(7)
-    ]
-    for entry in self.tape_slots_entries:
-      entry.pack(side="left")
-    self.tape_head_label = tk.Label(tape_frame, text="/\\")
-    self.tape_head_label.pack()
+    def build_tape_frame(self, parent):
+        tape_frame = tk.Frame(parent)
+        tape_frame.pack(pady=10)
+        tk.Label(tape_frame, text="Tape").pack()
+        tape_slots_frame = tk.Frame(tape_frame)
+        tape_slots_frame.pack()
+        self.tape_slots_entries = [
+            tk.Entry(tape_slots_frame, width=3) for _ in range(7)
+        ]
+        for entry in self.tape_slots_entries:
+            entry.pack(side="left")
+        self.tape_head_label = tk.Label(tape_frame, text="/\\")
+        self.tape_head_label.pack()
 
-  def build_debug_frame(self, parent):
-    debug_frame = tk.Frame(parent)
-    debug_frame.pack()
-    self.build_input_frame(debug_frame)
-    self.build_debug_button_frame(debug_frame)
-  
-  def build_input_frame(self, parent):
-    input_frame = tk.Frame(parent)
-    input_frame.pack(fill="x")
-    tk.Label(input_frame, text="Input:").pack(side="left")
-    self.input_entry = tk.Entry(input_frame)
-    self.input_entry.pack(side="left", fill="x")
+    def build_debug_frame(self, parent):
+        debug_frame = tk.Frame(parent)
+        debug_frame.pack()
+        self.build_input_frame(debug_frame)
+        self.build_debug_button_frame(debug_frame)
 
-  def build_debug_button_frame(self, parent):
-    debug_button_frame = tk.Frame(parent)
-    debug_button_frame.pack()
-    self.run_button = tk.Button(debug_button_frame, text="Run")
-    self.run_button.pack(side="left")
-    self.step_button = tk.Button(debug_button_frame, text="Step")
-    self.step_button.pack(side="left")
-    self.reset_button = tk.Button(debug_button_frame, text="Reset")
-    self.reset_button.pack(side="left")
-    self.stop_button = tk.Button(debug_button_frame, text="Stop")
-    self.stop_button.pack(side="left")
-    
-  def build_description_frame(self, parent):
-    description_frame = tk.Frame(parent)
-    description_frame.pack(fill="x", side="bottom", pady=20, padx=10)
-    self.alpha_label = tk.Label(description_frame, text="Alphabet = {a, b}", justify="left")
-    self.alpha_label.grid(row=0, column=0, sticky="w")
-    self.alpha_button = tk.Button(description_frame, text="Set Alphabet")
-    self.alpha_button.grid(row=1, column=0, sticky="w")
-    self.state_label = tk.Label(description_frame, text="Q = {q0, q1}", justify="left")
-    self.state_label.grid(row=2, column=0, sticky="w")
-    self.finals_label = tk.Label(description_frame, text="F = {q1}", justify="left")
-    self.finals_label.grid(row=3, column=0, sticky="w")
-    self.delta_label_1 = tk.Label(description_frame, text="delta(q0, a) = (q1, a, L)", justify="left")
-    self.delta_label_1.grid(row=4, column=0, sticky="w")
-    self.delta_label_2 = tk.Label(description_frame, text="delta(q1, b) = (q0, a, L)", justify="left")
-    self.delta_label_2.grid(row=5, column=0, sticky="w")
+    def build_input_frame(self, parent):
+        input_frame = tk.Frame(parent)
+        input_frame.pack(fill="x")
+        tk.Label(input_frame, text="Input:").pack(side="left")
+        self.input_entry = tk.Entry(input_frame)
+        self.input_entry.pack(side="left", fill="x")
 
-    ### TODO: There's some more stuff that needs to go in this window, but not sure what yet. ###
+    def build_debug_button_frame(self, parent):
+        debug_button_frame = tk.Frame(parent)
+        debug_button_frame.pack()
+        self.run_button = tk.Button(debug_button_frame, text="Run")
+        self.run_button.pack(side="left")
+        self.step_button = tk.Button(debug_button_frame, text="Step")
+        self.step_button.pack(side="left")
+        self.reset_button = tk.Button(debug_button_frame, text="Reset")
+        self.reset_button.pack(side="left")
+        self.stop_button = tk.Button(debug_button_frame, text="Stop")
+        self.stop_button.pack(side="left")
 
-  def run(self):
-    self.mainloop()
+    def build_description_frame(self, parent):
+        description_frame = tk.Frame(parent)
+        description_frame.pack(fill="x", side="bottom", pady=20, padx=10)
+        self.alpha_label = tk.Label(
+            description_frame, text="Alphabet = {a, b}", justify="left"
+        )
+        self.alpha_label.grid(row=0, column=0, sticky="w")
+        self.alpha_button = tk.Button(description_frame, text="Set Alphabet")
+        self.alpha_button.grid(row=1, column=0, sticky="w")
+        self.state_label = tk.Label(
+            description_frame, text="Q = {q0, q1}", justify="left"
+        )
+        self.state_label.grid(row=2, column=0, sticky="w")
+        self.finals_label = tk.Label(description_frame, text="F = {q1}", justify="left")
+        self.finals_label.grid(row=3, column=0, sticky="w")
+        self.delta_label_1 = tk.Label(
+            description_frame, text="delta(q0, a) = (q1, a, L)", justify="left"
+        )
+        self.delta_label_1.grid(row=4, column=0, sticky="w")
+        self.delta_label_2 = tk.Label(
+            description_frame, text="delta(q1, b) = (q0, a, L)", justify="left"
+        )
+        self.delta_label_2.grid(row=5, column=0, sticky="w")
+
+        ### TODO: There's some more stuff that needs to go in this window, but not sure what yet. ###
+
+    def run(self):
+        self.mainloop()
+
 
 def main():
-  editor = Window()
-  editor.run()
+    editor = Window()
+    editor.run()
+
 
 if __name__ == "__main__":
-  main()
+    main()
+
